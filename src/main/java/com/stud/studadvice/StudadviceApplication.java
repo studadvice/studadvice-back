32ca99044b3d215bfb3a1fbd279e42a909b473da
Churchill Atchedji <churchill.atchedji@limpidius.com>
2023-10-10 16:23:05 +0200

    feat(init) : initialisation du projet
    DEVSTUD-4

---
+++ src/main/java/com/stud/studadvice/StudadviceApplication.java
@@ -0,0 +1,13 @@
+package com.stud.studadvice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudadviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudadviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stud/studadvice/StudadviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stud.studadvice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudadviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
