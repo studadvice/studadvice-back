008994e237fbc4717540007d848d8b6361188721
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-11-04 12:57:12 +0100

    feat(prototype): add StudentDeals model

---
+++ src/main/java/com/stud/studadvice/model/deals/StudentDeals.java
@@ -0,0 +1,76 @@
+package com.stud.studadvice.model.deals;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document
+public class StudentDeals {
+    @Field("_id")
+    private ObjectId id;
+    private String title;
+    private String description;
+    private String category;
+    private String image;
+    private String startDate;
+    private String endDate;
+
+    public StudentDeals() {
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+}
