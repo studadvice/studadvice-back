45a62ace5342e2f2dbde3afd289fbe6f25864dc6
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-11-05 12:53:52 +0100

    fix(prototype-implementation): forgot setters and empty constructors for Informations model

--- src/main/java/com/stud/studadvice/model/administrative/Informations.java
+++ src/main/java/com/stud/studadvice/model/administrative/Informations.java
@@ -3,4 +3,23 @@ package com.stud.studadvice.model.administrative;
 public class Informations {
     private String title;
     private String description;
+
+    public Informations() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
