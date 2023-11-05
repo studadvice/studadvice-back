bb2714f6ff79485a3b1367c63b6a86ce7c2e0009
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-11-05 10:57:02 +0100

    feat(prototype-implementation): add administrative process exception

---
+++ src/main/java/com/stud/studadvice/exception/AdministrativeProcessException.java
@@ -0,0 +1,6 @@
+package com.stud.studadvice.exception;
+public class AdministrativeProcessException extends Exception {
+    public AdministrativeProcessException(String message){
+        super(message);
+    }
+}
