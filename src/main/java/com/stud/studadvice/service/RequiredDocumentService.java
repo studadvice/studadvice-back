a55db5d646f739597c376d089241e98a660dab9a
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-11-19 12:52:54 +0100

    feat(pagination): adapt services to the pagination

--- src/main/java/com/stud/studadvice/service/RequiredDocumentService.java
+++ src/main/java/com/stud/studadvice/service/RequiredDocumentService.java
@@ -1,23 +1,33 @@
 package com.stud.studadvice.service;
 
 import com.stud.studadvice.exception.RequiredDocumentException;
+import com.stud.studadvice.model.administrative.AdministrativeProcess;
 import com.stud.studadvice.model.administrative.RequiredDocument;
 import com.stud.studadvice.repository.administrative.RequiredDocumentRepository;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.TextCriteria;
+import org.springframework.data.mongodb.core.query.TextQuery;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class RequiredDocumentService {
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     @Autowired
     private RequiredDocumentRepository requiredDocumentRepository;
 
-    public List<RequiredDocument> getRequiredDocument() {
-        return requiredDocumentRepository.findAll();
+    public Page<RequiredDocument> getRequiredDocuments(Pageable pageable) {
+        return requiredDocumentRepository.findAll(pageable);
     }
 
     public RequiredDocument getRequiredDocumentById(ObjectId id) throws RequiredDocumentException {
@@ -45,5 +55,21 @@ public class RequiredDocumentService {
                 .orElseThrow(() -> new RequiredDocumentException("Required document not found"));
 
         requiredDocumentRepository.deleteById(id);
+    }
+
+    public Page<RequiredDocument> searchRequiredDocuments(String searchText, Pageable pageable) {
+        TextCriteria criteria = TextCriteria.forDefaultLanguage()
+                .matching(searchText);
+
+        Query query = TextQuery.queryText(criteria)
+                .sortByScore();
+
+        query.with(pageable);
+
+        long total = mongoTemplate.count(query, AdministrativeProcess.class);
+
+        List<RequiredDocument> processes = mongoTemplate.find(query, RequiredDocument.class);
+
+        return PageableExecutionUtils.getPage(processes, pageable, () -> total);
     }
 }
