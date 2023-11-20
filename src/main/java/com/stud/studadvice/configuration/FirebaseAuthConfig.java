af341235bd4c9c537229297ec95ec7a655af84b7
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-11-20 16:21:51 +0100

    refactor(security): update firebaseoptions to use GOOGLE_APPLICATION_CREDENTIALS in local

--- src/main/java/com/stud/studadvice/configuration/FirebaseAuthConfig.java
+++ src/main/java/com/stud/studadvice/configuration/FirebaseAuthConfig.java
@@ -4,31 +4,24 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
 import java.io.IOException;
-import java.io.InputStream;
+
 
 @Configuration
 public class FirebaseAuthConfig {
 
     @Bean
     public FirebaseAuth firebaseAuth() throws IOException {
-        InputStream serviceAccountStream = loadServiceAccountResource().getInputStream();
-
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccountStream))
+                .setCredentials(GoogleCredentials.getApplicationDefault())
                 .build();
 
         FirebaseApp.initializeApp(options);
 
         return FirebaseAuth.getInstance();
-    }
-
-    public Resource loadServiceAccountResource() {
-        return new ClassPathResource("serviceAccountKey.json");
     }
 }
