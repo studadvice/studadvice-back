73fe2f8a9d3e3a4892498ac9449b9bce4ddd1267
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-11-25 21:32:57 +0100

    docs(model-improvement): remove useless docs

--- src/main/java/com/stud/studadvice/controller/RequiredDocumentController.java
+++ src/main/java/com/stud/studadvice/controller/RequiredDocumentController.java
@@ -7,6 +7,7 @@ import com.stud.studadvice.service.RequiredDocumentService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -23,10 +24,6 @@ public class RequiredDocumentController {
     @Autowired
     private RequiredDocumentService requiredDocumentService;
 
-    /**
-     * Retrieves all the required document.
-     * @return A list of required documents.
-     */
     @Operation(summary = "Retrieve all required documents")
     @GetMapping
     private Page<RequiredDocument> getRequiredDocuments(@RequestParam(defaultValue = "${spring.data.web.pageable.default-page}") int page,
@@ -36,12 +33,6 @@ public class RequiredDocumentController {
         return requiredDocumentService.getRequiredDocuments(pageable);
     }
 
-    /**
-     * Retrieves a required document by its ID.
-     *
-     * @param requiredDocumentId The ID of the required document to retrieve.
-     * @return The required document with the specified ID.
-     */
     @Operation(summary = "Retrieve a required document by ID")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Required document retrieved successfully"),
@@ -57,36 +48,23 @@ public class RequiredDocumentController {
         }
     }
 
-    /**
-     * Creates a new required document .
-     *
-     * @param requiredDocument The required document to be created.
-     * @return The newly created required document.
-     */
     @Operation(summary = "Create a new required document")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Required document created successfully"),
             @ApiResponse(responseCode = "400", description = "Bad Request - Invalid input"),
     })
     @PostMapping
-    public RequiredDocument createRequiredDocument(@RequestBody RequiredDocument requiredDocument) {
+    public RequiredDocument createRequiredDocument(@Valid @RequestBody RequiredDocument requiredDocument) {
         return requiredDocumentService.createRequiredDocument(requiredDocument);
     }
 
-    /**
-     * Updates an existing required document.
-     *
-     * @param requiredDocumentId            The ID of the required document to update.
-     * @param requiredDocumentUpdated The updated required document data.
-     * @return The updated required document.
-     */
     @Operation(summary = "Update an existing required document")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Required document updated successfully"),
             @ApiResponse(responseCode = "404", description = "Required document not found"),
     })
     @PutMapping("/{requiredDocumentId}")
-    public RequiredDocument updateRequiredDocument(@PathVariable ObjectId requiredDocumentId, @RequestBody RequiredDocument requiredDocumentUpdated) {
+    public RequiredDocument updateRequiredDocument(@PathVariable ObjectId requiredDocumentId, @Valid @RequestBody RequiredDocument requiredDocumentUpdated) {
         try {
             return requiredDocumentService.updateRequiredDocument(requiredDocumentId,requiredDocumentUpdated);
         }
@@ -95,11 +73,6 @@ public class RequiredDocumentController {
         }
     }
 
-    /**
-     * Deletes a required document by its ID.
-     *
-     * @param requiredDocumentId The unique ID of the required document to delete.
-     */
     @Operation(summary = "Delete a required document process by ID")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "204", description = "Required document deleted successfully"),
@@ -115,12 +88,6 @@ public class RequiredDocumentController {
         }
     }
 
-    /**
-     * Searches for required documents based on a text search.
-     *
-     * @param searchText The text to search for in the required documents.
-     * @return A list of the required documents matching the search criteria.
-     */
     @Operation(summary = "Search for required documents by text")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Required documents retrieved successfully"),
