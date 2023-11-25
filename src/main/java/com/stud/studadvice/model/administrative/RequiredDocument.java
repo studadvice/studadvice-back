282ab4ac8eaf67f4d10af67ad4987dc8cde9d15f
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-11-25 21:32:28 +0100

    fix(model-improvement): add some annotations for step, resource and RequiredDocument models

--- src/main/java/com/stud/studadvice/model/administrative/RequiredDocument.java
+++ src/main/java/com/stud/studadvice/model/administrative/RequiredDocument.java
@@ -3,6 +3,7 @@ package com.stud.studadvice.model.administrative;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.bson.types.ObjectId;
@@ -17,10 +18,13 @@ public class RequiredDocument {
     @JsonSerialize(using = ToStringSerializer.class)
     private ObjectId id;
     @TextIndexed
+    @NotNull(message = "Please, give a name for your required document")
     private String name;
     @TextIndexed
+    @NotNull(message = "Please, give a description for your required document")
     private String description;
     private String image;
+    private String url;
     public RequiredDocument() {
     }
 }
