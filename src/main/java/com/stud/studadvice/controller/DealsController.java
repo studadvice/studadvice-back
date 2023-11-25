c1dcb7d3a60858706aefc382be121678bed2f3d4
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-11-25 21:32:57 +0100

    docs(model-improvement): remove useless docs

--- src/main/java/com/stud/studadvice/controller/DealsController.java
+++ src/main/java/com/stud/studadvice/controller/DealsController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
+import jakarta.validation.Valid;
 import org.bson.types.ObjectId;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,11 +31,6 @@ public class DealsController {
     @Autowired
     private DealsService dealsService;
 
-    /**
-     * Retrieves a list of all student deals.
-     *
-     * @return A list of student deals.
-     */
     @Operation(summary = "Retrieve a list of all student deals")
     @GetMapping
     public Page<Deal> getDeals(@RequestParam(defaultValue = "${spring.data.web.pageable.default-page}") int page,
@@ -44,12 +40,6 @@ public class DealsController {
         return dealsService.getDeals(pageable);
     }
 
-    /**
-     * Retrieves a deal by its ID.
-     *
-     * @param dealId The ID of the deal to retrieve.
-     * @return The deals with the specified ID.
-     */
     @Operation(summary = "Retrieve a deal by ID")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Deal retrieved successfully"),
@@ -65,12 +55,6 @@ public class DealsController {
     }
 
 
-    /**
-     * Creates a new student deal.
-     *
-     * @param newDeal The student deal to be created.
-     * @return The newly created student deal.
-     */
     @Operation(summary = "Create a new student deal")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Student deal created successfully"),
@@ -78,17 +62,10 @@ public class DealsController {
     })
     @PostMapping
     @PreAuthorize("hasAuthority('ADMIN')")
-    public Deal createDeal(@RequestBody Deal newDeal) {
+    public Deal createDeal(@Valid @RequestBody Deal newDeal) {
         return dealsService.createDeal(newDeal);
     }
 
-    /**
-     * Updates an existing student deal.
-     *
-     * @param dealId           The unique ID of the student deal to update.
-     * @param updatedDeal The updated student deal data.
-     * @return The updated student deal.
-     */
     @Operation(summary = "Update an existing student deal by ID")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Student deal updated successfully"),
@@ -96,7 +73,7 @@ public class DealsController {
     })
     @PutMapping("/{dealId}")
     @PreAuthorize("hasAuthority('ADMIN')")
-    public Deal updateDeal(@PathVariable ObjectId dealId, @RequestBody Deal updatedDeal) {
+    public Deal updateDeal(@PathVariable ObjectId dealId, @Valid @RequestBody Deal updatedDeal) {
         try {
             return dealsService.updateDeal(dealId, updatedDeal);
         } catch (DealException dealException) {
@@ -104,11 +81,6 @@ public class DealsController {
         }
     }
 
-    /**
-     * Deletes a student deal by its unique ID.
-     *
-     * @param dealId The unique ID of the student deal to delete.
-     */
     @Operation(summary = "Delete a student deal by ID")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "204", description = "Student deal deleted successfully"),
@@ -124,12 +96,6 @@ public class DealsController {
         }
     }
 
-    /**
-     * Searches for deals based on a text search.
-     *
-     * @param searchText The text to search for in the deals.
-     * @return A list of the deals matching the search criteria.
-     */
     @Operation(summary = "Search for deals by text")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Deals retrieved successfully"),
--- src/main/java/com/stud/studadvice/controller/RequiredDocumentController.java
+++ src/main/java/com/stud/studadvice/controller/RequiredDocumentController.java
@@ -7,6 +7,7 @@ import com.stud.studadvice.service.RequiredDocumentService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -24,10 +25,6 @@ public class RequiredDocumentController {
     @Autowired
     private RequiredDocumentService requiredDocumentService;
 
-    /**
-     * Retrieves all the required document.
-     * @return A list of required documents.
-     */
     @Operation(summary = "Retrieve all required documents")
     @GetMapping
     private Page<RequiredDocument> getRequiredDocuments(@RequestParam(defaultValue = "${spring.data.web.pageable.default-page}") int page,
@@ -37,12 +34,6 @@ public class RequiredDocumentController {
         return requiredDocumentService.getRequiredDocuments(pageable);
     }
 
-    /**
-     * Retrieves a required document by its ID.
-     *
-     * @param requiredDocumentId The ID of the required document to retrieve.
-     * @return The required document with the specified ID.
-     */
     @Operation(summary = "Retrieve a required document by ID")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Required document retrieved successfully"),
@@ -58,12 +49,6 @@ public class RequiredDocumentController {
         }
     }
 
-    /**
-     * Creates a new required document .
-     *
-     * @param requiredDocument The required document to be created.
-     * @return The newly created required document.
-     */
     @Operation(summary = "Create a new required document")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Required document created successfully"),
@@ -71,17 +56,10 @@ public class RequiredDocumentController {
     })
     @PostMapping
     @PreAuthorize("hasAuthority('ADMIN')")
-    public RequiredDocument createRequiredDocument(@RequestBody RequiredDocument requiredDocument) {
+    public RequiredDocument createRequiredDocument(@Valid @RequestBody RequiredDocument requiredDocument) {
         return requiredDocumentService.createRequiredDocument(requiredDocument);
     }
 
-    /**
-     * Updates an existing required document.
-     *
-     * @param requiredDocumentId            The ID of the required document to update.
-     * @param requiredDocumentUpdated The updated required document data.
-     * @return The updated required document.
-     */
     @Operation(summary = "Update an existing required document")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Required document updated successfully"),
@@ -89,7 +67,7 @@ public class RequiredDocumentController {
     })
     @PutMapping("/{requiredDocumentId}")
     @PreAuthorize("hasAuthority('ADMIN')")
-    public RequiredDocument updateRequiredDocument(@PathVariable ObjectId requiredDocumentId, @RequestBody RequiredDocument requiredDocumentUpdated) {
+    public RequiredDocument updateRequiredDocument(@PathVariable ObjectId requiredDocumentId, @Valid @RequestBody RequiredDocument requiredDocumentUpdated) {
         try {
             return requiredDocumentService.updateRequiredDocument(requiredDocumentId,requiredDocumentUpdated);
         }
@@ -98,11 +76,6 @@ public class RequiredDocumentController {
         }
     }
 
-    /**
-     * Deletes a required document by its ID.
-     *
-     * @param requiredDocumentId The unique ID of the required document to delete.
-     */
     @Operation(summary = "Delete a required document process by ID")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "204", description = "Required document deleted successfully"),
@@ -119,12 +92,6 @@ public class RequiredDocumentController {
         }
     }
 
-    /**
-     * Searches for required documents based on a text search.
-     *
-     * @param searchText The text to search for in the required documents.
-     * @return A list of the required documents matching the search criteria.
-     */
     @Operation(summary = "Search for required documents by text")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Required documents retrieved successfully"),
