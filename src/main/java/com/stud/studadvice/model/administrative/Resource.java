4b34cb7934aea4420139ee01a4e5c9495036720e
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-11-25 21:32:28 +0100

    fix(model-improvement): add some annotations for step, resource and RequiredDocument models

--- src/main/java/com/stud/studadvice/model/administrative/Resource.java
+++ src/main/java/com/stud/studadvice/model/administrative/Resource.java
@@ -1,14 +1,17 @@
 package com.stud.studadvice.model.administrative;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class Resource {
+    @NotNull(message = "Please, give a name for your resource")
     private String name;
     private String url;
     private String image;
+    @NotNull(message = "Please, give a description for your resource")
     private String description;
 
     public Resource() {
