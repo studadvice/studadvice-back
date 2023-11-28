42fe3a41a0689f2f12b454ab0877f3ef54fdf978
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-11-28 09:59:28 +0100

    feat: add file service, controller and model

---
+++ src/main/java/com/stud/studadvice/controller/FileController.java
@@ -0,0 +1,41 @@
+package com.stud.studadvice.controller;
+
+import com.stud.studadvice.model.File;
+import com.stud.studadvice.service.FileService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+
+@RestController
+public class FileController {
+    @Autowired
+    private FileService imageService;
+
+    @GetMapping("/download/{id}")
+    public ResponseEntity<ByteArrayResource> download(@PathVariable String id){
+        try {
+            File file = imageService.download(id);
+            if (imageService.checkFile(file)) {
+                return ResponseEntity.ok()
+                        .contentType(MediaType.parseMediaType(file.getFileType()))
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
+                        .body(new ByteArrayResource(file.getFile()));
+            } else {
+                return null;
+            }
+        }
+        catch (IOException ioException){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ioException.getMessage(), ioException);
+        }
+    }
+}
