0b3d9df0b01c319661787d2ae67dbba890e4a7fa
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-11-28 17:34:47 +0100

    feat: init of the services tests (will do it again because of the "new architecture")

--- src/test/java/com/stud/studadvice/service/CategoryServiceTest.java
+++ src/test/java/com/stud/studadvice/service/CategoryServiceTest.java
@@ -1,32 +1,41 @@
 package com.stud.studadvice.service;
 
-import com.stud.studadvice.exception.CategoryException;
-import com.stud.studadvice.exception.AdministrativeProcessException;
+import com.stud.studadvice.dto.AdministrativeProcessDto;
+import com.stud.studadvice.dto.CategoryDto;
 import com.stud.studadvice.entity.AdministrativeProcess;
 import com.stud.studadvice.entity.Category;
+import com.stud.studadvice.exception.AdministrativeProcessException;
+import com.stud.studadvice.exception.CategoryException;
+import com.stud.studadvice.exception.ImageException;
 import com.stud.studadvice.repository.administrative.AdministrativeProcessRepository;
 import com.stud.studadvice.repository.categories.CategoryRepository;
-
 import org.bson.types.ObjectId;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
+import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.util.Collections;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 public class CategoryServiceTest {
+
+    @Mock
+    private ModelMapper modelMapper;
+
+    @Mock
+    private GridFsTemplate gridFsTemplate;
+
     @Mock
     private CategoryRepository categoryRepository;
 
@@ -37,221 +46,91 @@ public class CategoryServiceTest {
     private CategoryService categoryService;
 
     @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
+    public void setUp() {
     }
 
     @Test
-    void getCategories_WhenCategoriesExist_ShouldReturnCategoriesPage() {
-        // Given
-        int page = 0;
-        int size = 10;
-        Pageable pageable = Pageable.ofSize(size).withPage(page);
-
-        Page<Category> expectedPage = mock(Page.class);
-        when(categoryRepository.findAll(pageable)).thenReturn(expectedPage);
-
-        // When
-        Page<Category> actualPage = categoryService.getCategories(pageable);
-
-        // Then
-        assertNotNull(actualPage);
-        assertEquals(expectedPage, actualPage);
+    void testGetCategories_ShouldReturnPage() {
+        // TODO
     }
 
     @Test
-    void getCategoryById_WhenCategoryExists_ShouldReturnCategory() throws CategoryException {
-        // Given
-        ObjectId categoryId = new ObjectId("6035c09333d5390c6a618f4a");
-        Category expectedCategory = new Category();
-        when(categoryRepository.findById(categoryId)).thenReturn(Optional.of(expectedCategory));
-
-        // When
-        Category actualCategory = categoryService.getCategoryById(categoryId);
-
-        // Then
-        assertNotNull(actualCategory);
-        assertEquals(expectedCategory, actualCategory);
+    void testGetCategoryById_ExistingId_ShouldReturnDto() {
+        // TODO
     }
 
     @Test
-    void getCategoryById_WhenCategoryDoesNotExist_ShouldThrowException() {
-        // Given
-        ObjectId categoryId = new ObjectId("6035c09333d5390c6a618f4a");
-        when(categoryRepository.findById(categoryId)).thenReturn(Optional.empty());
-
-        // When, Then
-        assertThrows(CategoryException.class,
-                () -> categoryService.getCategoryById(categoryId));
-    }
-
-//    @Test
-//    void createCategory_WhenValidCategory_ShouldReturnCreatedCategory() throws AdministrativeProcessException {
-//        // Given
-//        Category inputCategory = new Category();
-//        Category expectedCategory = new Category();
-//        when(administrativeProcessRepository.findById(any())).thenReturn(Optional.of(new AdministrativeProcess()));
-//        when(categoryRepository.save(inputCategory)).thenReturn(expectedCategory);
-//
-//        // When
-//        Category actualCategory = categoryService.createCategory(inputCategory);
-//
-//        // Then
-//        assertNotNull(actualCategory);
-//        assertEquals(expectedCategory, actualCategory);
-//    }
-//
-//    @Test
-//    void createCategory_WhenInvalidCategory_ShouldThrowException() {
-//        // Given
-//        Category inputCategory = new Category();
-//        when(administrativeProcessRepository.findById(any())).thenReturn(Optional.of(new AdministrativeProcess()));
-//        when(categoryRepository.save(inputCategory)).thenThrow(new RuntimeException("Test exception"));
-//
-//        // When, Then
-//        assertThrows(RuntimeException.class,
-//                () -> categoryService.createCategory(inputCategory));
-//    }
-//
-//    @Test
-//    void updateCategoryById_WhenValidInput_ShouldReturnUpdatedCategory() throws CategoryException, AdministrativeProcessException {
-//        // Given
-//        ObjectId categoryId = new ObjectId("6035c09333d5390c6a618f4a");
-//        Category existingCategory = new Category();
-//        Category updatedCategory = new Category();
-//        updatedCategory.setImage("New Image");
-//        updatedCategory.setDescription("New Description");
-//
-//        when(categoryRepository.findById(categoryId)).thenReturn(Optional.of(existingCategory));
-//        when(administrativeProcessRepository.findById(any())).thenReturn(Optional.of(new AdministrativeProcess()));
-//        when(categoryRepository.save(existingCategory)).thenReturn(updatedCategory);
-//
-//        // When
-//        Category actualCategory = categoryService.updateCategoryById(categoryId, updatedCategory);
-//
-//        // Then
-//        assertNotNull(actualCategory);
-//        assertEquals(updatedCategory, actualCategory);
-//        assertEquals("New Image", actualCategory.getImage());
-//        assertEquals("New Description", actualCategory.getDescription());
-//    }
-//
-//    @Test
-//    void updateCategoryById_WhenCategoryDoesNotExist_ShouldThrowException() {
-//        // Given
-//        ObjectId categoryId = new ObjectId("6035c09333d5390c6a618f4a");
-//        Category updatedCategory = new Category();
-//        when(categoryRepository.findById(categoryId)).thenReturn(Optional.empty());
-//
-//        // When, Then
-//        assertThrows(CategoryException.class,
-//                () -> categoryService.updateCategoryById(categoryId, updatedCategory));
-//    }
-
-    @Test
-    void deleteCategoryById_WhenCategoryExists_ShouldDeleteCategory() {
-        // Given
-        ObjectId categoryId = new ObjectId("6035c09333d5390c6a618f4a");
-        Category existingCategory = new Category();
-        when(categoryRepository.findById(categoryId)).thenReturn(Optional.of(existingCategory));
-        when(categoryRepository.existsById(categoryId)).thenReturn(true);
-
-        // When
-        assertDoesNotThrow(() -> categoryService.deleteCategoryById(categoryId));
-
-        // Then: No exception should be thrown, and the delete method should be called
-        verify(categoryRepository).delete(existingCategory);
+    void testGetCategoryById_NonExistingId_ShouldThrowException() {
+        // TODO
     }
 
     @Test
-    void deleteCategoryById_WhenCategoryDoesNotExist_ShouldThrowException() {
-        // Given
-        ObjectId categoryId = new ObjectId("6035c09333d5390c6a618f4a");
-        when(categoryRepository.findById(categoryId)).thenReturn(Optional.empty());
-
-        // When, Then
-        assertThrows(CategoryException.class,
-                () -> categoryService.deleteCategoryById(categoryId));
+    void testCreateCategory_ValidData_ShouldReturnDto() {
+        // TODO
     }
 
     @Test
-    void getAdministrativeProcessByCategoryId_WhenCategoryExists_ShouldReturnAdministrativeProcessesPage() throws CategoryException {
-        // Given
-        ObjectId categoryId = new ObjectId("6035c09333d5390c6a618f4a");
-        Category category = new Category();
-        when(categoryRepository.findById(categoryId)).thenReturn(Optional.of(category));
-
-        List<AdministrativeProcess> administrativeProcesses = new ArrayList<>();
-        administrativeProcesses.add(new AdministrativeProcess());
-        category.setAdministrativeProcesses(administrativeProcesses);
-
-        when(categoryRepository.findById(categoryId)).thenReturn(Optional.of(category));
-
-        // When
-        Page<AdministrativeProcess> actualPage = categoryService.getAdministrativeProcessByCategoryId(categoryId, null, null, null, null, Pageable.unpaged());
-
-        // Then
-        assertNotNull(actualPage);
-        assertEquals(administrativeProcesses, actualPage.getContent());
+    void testCreateCategory_InvalidData_ShouldThrowException() {
+        // TODO
     }
 
     @Test
-    void getAdministrativeProcessByCategoryId_WhenCategoryDoesNotExist_ShouldThrowException() {
-        // Given
-        ObjectId categoryId = new ObjectId("6035c09333d5390c6a618f4a");
-        when(categoryRepository.findById(categoryId)).thenReturn(Optional.empty());
-
-        // When, Then
-        assertThrows(CategoryException.class,
-                () -> categoryService.getAdministrativeProcessByCategoryId(categoryId, null, null, null, null, Pageable.unpaged()));
+    void testUpdateCategoryById_ExistingIdAndValidData_ShouldReturnDto() {
+        // TODO
     }
 
     @Test
-    void addAdministrativeProcessToAnExistingCategory_WhenValidInput_ShouldReturnUpdatedCategory() throws CategoryException, AdministrativeProcessException {
-        // Given
-        ObjectId categoryId = new ObjectId("6035c09333d5390c6a618f4a");
-        ObjectId administrativeProcessId = new ObjectId("6035c09333d5390c6a618f4b");
-        Category existingCategory = new Category();
-        AdministrativeProcess existingAdministrativeProcess = new AdministrativeProcess();
-
-        when(categoryRepository.findById(categoryId)).thenReturn(Optional.of(existingCategory));
-        when(administrativeProcessRepository.findById(administrativeProcessId)).thenReturn(Optional.of(existingAdministrativeProcess));
-        when(categoryRepository.save(existingCategory)).thenReturn(existingCategory);
-
-        // When
-        Category actualCategory = categoryService.addAdministrativeProcessToAnExistingCategory(categoryId, administrativeProcessId);
-
-        // Then
-        assertNotNull(actualCategory);
-        assertEquals(existingCategory, actualCategory);
-        assertEquals(1, actualCategory.getAdministrativeProcesses().size());
-        assertEquals(existingAdministrativeProcess, actualCategory.getAdministrativeProcesses().get(0));
+    void testUpdateCategoryById_NonExistingId_ShouldThrowException() {
+        // TODO
     }
 
     @Test
-    void addAdministrativeProcessToAnExistingCategory_WhenCategoryDoesNotExist_ShouldThrowCategoryException() {
-        // Given
-        ObjectId categoryId = new ObjectId("6035c09333d5390c6a618f4a");
-        ObjectId administrativeProcessId = new ObjectId("6035c09333d5390c6a618f4b");
-        when(categoryRepository.findById(categoryId)).thenReturn(Optional.empty());
-
-        // When, Then
-        assertThrows(CategoryException.class,
-                () -> categoryService.addAdministrativeProcessToAnExistingCategory(categoryId, administrativeProcessId));
+    void testUpdateCategoryById_InvalidData_ShouldThrowException() {
+        // TODO
     }
 
     @Test
-    void addAdministrativeProcessToAnExistingCategory_WhenAdministrativeProcessDoesNotExist_ShouldThrowAdministrativeProcessException() {
-        // Given
-        ObjectId categoryId = new ObjectId("6035c09333d5390c6a618f4a");
-        ObjectId administrativeProcessId = new ObjectId("6035c09333d5390c6a618f4b");
-        Category existingCategory = new Category();
+    void testDeleteCategoryById_ExistingId_ShouldNotThrowException() {
+    // TODO
+    }
 
-        when(categoryRepository.findById(categoryId)).thenReturn(Optional.of(existingCategory));
-        when(administrativeProcessRepository.findById(administrativeProcessId)).thenReturn(Optional.empty());
+    @Test
+    void testDeleteCategoryById_NonExistingId_ShouldThrowException() {
+        // TODO
+    }
 
-        // When, Then
-        assertThrows(AdministrativeProcessException.class,
-                () -> categoryService.addAdministrativeProcessToAnExistingCategory(categoryId, administrativeProcessId));
+    @Test
+    void testGetAdministrativeProcessByCategoryId_WithSearchParams_ShouldReturnPage() {
+        // TODO
+    }
+
+    @Test
+    void testGetAdministrativeProcessByCategoryId_WithNoSearchParams_ShouldReturnPage() {
+        // TODO
+    }
+
+    @Test
+    void testAddAdministrativeProcessToAnExistingCategory_ExistingIds_ShouldReturnDto() {
+        // TODO
+    }
+
+    @Test
+    void testAddAdministrativeProcessToAnExistingCategory_NonExistingCategory_ShouldThrowException() {
+        // TODO
+    }
+
+    @Test
+    void testAddAdministrativeProcessToAnExistingCategory_NonExistingProcess_ShouldThrowException() {
+        // TODO
+    }
+
+    @Test
+    void testStoreImage_ValidImageFile_ShouldReturnImageId() {
+        // TODO
+    }
+
+    @Test
+    void testStoreImage_InvalidImageFile_ShouldThrowException() {
+        // TODO
     }
 }
--- src/test/java/com/stud/studadvice/service/RequiredDocumentServiceTest.java
+++ src/test/java/com/stud/studadvice/service/RequiredDocumentServiceTest.java
@@ -1,219 +1,114 @@
 package com.stud.studadvice.service;
 
-import com.stud.studadvice.exception.RequiredDocumentException;
+import com.stud.studadvice.dto.RequiredDocumentDto;
 import com.stud.studadvice.entity.RequiredDocument;
+import com.stud.studadvice.exception.ImageException;
+import com.stud.studadvice.exception.RequiredDocumentException;
 import com.stud.studadvice.repository.administrative.RequiredDocumentRepository;
-
 import org.bson.types.ObjectId;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
+import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.TextCriteria;
-import org.springframework.data.mongodb.core.query.TextQuery;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.util.Collections;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 public class RequiredDocumentServiceTest {
+
     @Mock
-    private MongoTemplate mongoTemplate;
+    private GridFsTemplate gridFsTemplate;
+
     @Mock
     private RequiredDocumentRepository requiredDocumentRepository;
+
+    @Mock
+    private ModelMapper modelMapper;
 
     @InjectMocks
     private RequiredDocumentService requiredDocumentService;
 
     @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
+    public void setUp() {
     }
 
     @Test
-    void getRequiredDocuments_WhenDocumentsExist_ShouldReturnDocumentsPage() {
-        // Given
-        List<RequiredDocument> documentsList = new ArrayList<>();
-        documentsList.add(new RequiredDocument());
-        documentsList.add(new RequiredDocument());
-
-        Page<RequiredDocument> expectedPage = new PageImpl<>(documentsList);
-
-        when(requiredDocumentRepository.findAll(Pageable.unpaged())).thenReturn(expectedPage);
-
-        // When
-        Page<RequiredDocument> actualPage = requiredDocumentService.getRequiredDocuments(Pageable.unpaged());
-
-        // Then
-        assertNotNull(actualPage);
-        assertEquals(expectedPage, actualPage);
+    void testGetRequiredDocuments_ShouldReturnPage() {
+        // TODO
     }
 
     @Test
-    void getRequiredDocuments_WhenNoDocumentsExist_ShouldReturnEmptyPage() {
-        // Given
-
-        when(requiredDocumentRepository.findAll(Pageable.unpaged())).thenReturn(Page.empty());
-
-        // When
-        Page<RequiredDocument> actualPage = requiredDocumentService.getRequiredDocuments(Pageable.unpaged());
-
-        // Then
-        assertNotNull(actualPage);
-        assertTrue(actualPage.isEmpty());
+    void testGetRequiredDocumentById_ExistingId_ShouldReturnDto() {
+        // TODO
     }
 
     @Test
-    void getRequiredDocumentById_WhenDocumentExists_ShouldReturnDocument() throws RequiredDocumentException {
-        // Given
-        ObjectId documentId = new ObjectId("6035c09333d5390c6a618f4a");
-        RequiredDocument expectedDocument = new RequiredDocument();
-        when(requiredDocumentRepository.findById(documentId)).thenReturn(Optional.of(expectedDocument));
-
-        // When
-        RequiredDocument actualDocument = requiredDocumentService.getRequiredDocumentById(documentId);
-
-        // Then
-        assertNotNull(actualDocument);
-        assertEquals(expectedDocument, actualDocument);
+    void testGetRequiredDocumentById_NonExistingId_ShouldThrowException() {
+        // TODO
     }
 
     @Test
-    void getRequiredDocumentById_WhenDocumentDoesNotExist_ShouldThrowException() {
-        // Given
-        ObjectId documentId = new ObjectId("6035c09333d5390c6a618f4a");
-        when(requiredDocumentRepository.findById(documentId)).thenReturn(Optional.empty());
-
-        // When, Then
-        assertThrows(RequiredDocumentException.class,
-                () -> requiredDocumentService.getRequiredDocumentById(documentId));
-    }
-
-//    @Test
-//    void createRequiredDocument_WhenValidDocument_ShouldReturnCreatedDocument() {
-//        // Given
-//        RequiredDocument inputDocument = new RequiredDocument();
-//        RequiredDocument expectedDocument = new RequiredDocument();
-//        when(requiredDocumentRepository.save(inputDocument)).thenReturn(expectedDocument);
-//
-//        // When
-//        RequiredDocument actualDocument = requiredDocumentService.createRequiredDocument(inputDocument);
-//
-//        // Then
-//        assertNotNull(actualDocument);
-//        assertEquals(expectedDocument, actualDocument);
-//    }
-//
-//    @Test
-//    void updateRequiredDocument_WhenDocumentExists_ShouldReturnUpdatedDocument() throws RequiredDocumentException {
-//        // Given
-//        ObjectId documentId = new ObjectId("6035c09333d5390c6a618f4a");
-//        RequiredDocument existingDocument = new RequiredDocument();
-//        when(requiredDocumentRepository.findById(documentId)).thenReturn(Optional.of(existingDocument));
-//
-//        RequiredDocument updatedDocument = new RequiredDocument();
-//
-//        when(requiredDocumentRepository.save(existingDocument)).thenReturn(updatedDocument);
-//
-//        // When
-//        RequiredDocument actualDocument = requiredDocumentService.updateRequiredDocument(documentId, updatedDocument);
-//
-//        // Then
-//        assertNotNull(actualDocument);
-//        assertEquals(updatedDocument, actualDocument);
-//    }
-//
-//    @Test
-//    void updateRequiredDocument_WhenDocumentDoesNotExist_ShouldThrowException() {
-//        // Given
-//        ObjectId documentId = new ObjectId("6035c09333d5390c6a618f4a");
-//        when(requiredDocumentRepository.findById(documentId)).thenReturn(Optional.empty());
-//
-//        RequiredDocument updatedDocument = new RequiredDocument();
-//
-//        // When, Then
-//        assertThrows(RequiredDocumentException.class,
-//                () -> requiredDocumentService.updateRequiredDocument(documentId, updatedDocument));
-//    }
-
-    @Test
-    void deleteRequiredDocument_WhenDocumentExists_ShouldDeleteDocument() {
-        // Given
-        ObjectId documentId = new ObjectId("6035c09333d5390c6a618f4a");
-        RequiredDocument existingDocument = new RequiredDocument();
-        when(requiredDocumentRepository.findById(documentId)).thenReturn(Optional.of(existingDocument));
-
-        // When
-        assertDoesNotThrow(() -> requiredDocumentService.deleteRequiredDocument(documentId));
-
-        // Then: No exception should be thrown
-        verify(requiredDocumentRepository).delete(existingDocument);
+    void testCreateRequiredDocument_ValidData_ShouldReturnDto() {
+        // TODO
     }
 
     @Test
-    void deleteRequiredDocument_WhenDocumentDoesNotExist_ShouldThrowException() {
-        // Given
-        ObjectId documentId = new ObjectId("6035c09333d5390c6a618f4a");
-        when(requiredDocumentRepository.findById(documentId)).thenReturn(Optional.empty());
-
-        // When, Then
-        assertThrows(RequiredDocumentException.class,
-                () -> requiredDocumentService.deleteRequiredDocument(documentId));
+    void testCreateRequiredDocument_InvalidData_ShouldThrowException() {
+        // TODO
     }
 
     @Test
-    void searchRequiredDocuments_WhenResultsExist_ShouldReturnResultsPage() {
-        // Given
-        String searchText = "example";
-
-        List<RequiredDocument> documentsList = new ArrayList<>();
-        documentsList.add(new RequiredDocument());
-        documentsList.add(new RequiredDocument());
-
-        Page<RequiredDocument> expectedPage = new PageImpl<>(documentsList);
-
-        TextCriteria criteria = TextCriteria.forDefaultLanguage().matching(searchText);
-        Query query = TextQuery.queryText(criteria).sortByScore().with(Pageable.unpaged());
-
-        when(mongoTemplate.count(query, RequiredDocument.class)).thenReturn(expectedPage.getTotalElements());
-        when(mongoTemplate.find(query, RequiredDocument.class)).thenReturn(documentsList);
-
-        // When
-        Page<RequiredDocument> actualPage = requiredDocumentService.searchRequiredDocuments(searchText, Pageable.unpaged());
-
-        // Then
-        assertNotNull(actualPage);
-        assertEquals(expectedPage, actualPage);
+    void testUpdateRequiredDocument_ExistingIdAndValidData_ShouldReturnDto() {
+        // TODO
     }
 
     @Test
-    void searchRequiredDocuments_WhenNoResultsExist_ShouldReturnEmptyPage() {
-        // Given
-        String searchText = "nonexistent";
+    void testUpdateRequiredDocument_NonExistingId_ShouldThrowException() {
+        // TODO
+    }
 
-        TextCriteria criteria = TextCriteria.forDefaultLanguage().matching(searchText);
-        Query query = TextQuery.queryText(criteria).sortByScore().with(Pageable.unpaged());
+    @Test
+    void testUpdateRequiredDocument_InvalidData_ShouldThrowException() {
+        // TODO
+    }
 
-        when(mongoTemplate.count(query, RequiredDocument.class)).thenReturn(0L);
+    @Test
+    void testDeleteRequiredDocument_ExistingId_ShouldNotThrowException() {
+        // TODO
+    }
 
-        // When
-        Page<RequiredDocument> actualPage = requiredDocumentService.searchRequiredDocuments(searchText, Pageable.unpaged());
+    @Test
+    void testDeleteRequiredDocument_NonExistingId_ShouldThrowException() {
+        // TODO
+    }
 
-        // Then
-        assertNotNull(actualPage);
-        assertTrue(actualPage.isEmpty());
+    @Test
+    void testSearchRequiredDocuments_WithSearchParams_ShouldReturnPage() {
+        // TODO
+    }
+
+    @Test
+    void testSearchRequiredDocuments_WithNoSearchParams_ShouldReturnPage() {
+        // TODO
+    }
+
+    @Test
+    void testStoreImage_ValidImageFile_ShouldReturnImageId() {
+        // TODO
+    }
+
+    @Test
+    void testStoreImage_InvalidImageFile_ShouldThrowException() {
+        // TODO
     }
 }
