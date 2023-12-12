872f66e3395f7f81d3430eb1f3d513316582d4ff
amevigbe <yaoisidore.amevigbe.etu@univ-lille.fr>
2023-12-12 11:36:30 +0100

    feat[workflows](deploy) : Correction de l'action deploy
    DEVSTUD-4

--- src/main/java/com/stud/studadvice/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/stud/studadvice/configuration/WebSecurityConfiguration.java
@@ -22,6 +22,7 @@ public class WebSecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                .cors().and()
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(antMatcher("/swagger-ui.html"),
                                 antMatcher("/swagger-ui/**"),
