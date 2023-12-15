21a2b9bf31df29f038aa191f17be819db4f80f18
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-12-15 22:00:12 +0100

    refactor: remove useless code

--- src/test/java/com/stud/studadvice/service/RequiredDocumentServiceTest.java
+++ src/test/java/com/stud/studadvice/service/RequiredDocumentServiceTest.java
@@ -2,35 +2,35 @@ package com.stud.studadvice.service;
 
 import com.stud.studadvice.dto.RequiredDocumentDto;
 import com.stud.studadvice.entity.RequiredDocument;
-import com.stud.studadvice.exception.ImageException;
 import com.stud.studadvice.exception.RequiredDocumentException;
 import com.stud.studadvice.repository.administrative.RequiredDocumentRepository;
+
 import org.bson.types.ObjectId;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+
 import org.mockito.MockitoAnnotations;
+
 import org.modelmapper.ModelMapper;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.mongodb.gridfs.GridFsTemplate;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 public class RequiredDocumentServiceTest {
-
-    @Mock
-    private GridFsTemplate gridFsTemplate;
 
     @Mock
     private RequiredDocumentRepository requiredDocumentRepository;
@@ -115,9 +115,7 @@ public class RequiredDocumentServiceTest {
 
         when(requiredDocumentRepository.findById(id)).thenReturn(Optional.empty());
 
-        assertThrows(RequiredDocumentException.class, () -> {
-            requiredDocumentService.deleteRequiredDocument(id);
-        });
+        assertThrows(RequiredDocumentException.class, () -> requiredDocumentService.deleteRequiredDocument(id));
 
         verify(requiredDocumentRepository, never()).delete(any());
     }
